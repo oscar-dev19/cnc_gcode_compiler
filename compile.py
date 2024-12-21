594727996078795eac4589c9c6acbfee8cb6a861
Oscar Lopez <oscarlopez@Oscars-MacBook-Air.local>
2024-12-21 10:10:28 -0800

    altered compiler to take in program as argument thast runs the gcode code instead of a hardcoded value in the compiler.

--- compile.py
+++ compile.py
@@ -3,14 +3,21 @@ from gcodeLexer import gcodeLexer
 from gcodeParser import gcodeParser
 from gcodeVisitor import gcodeVisitor
 import time
+import argparse
 
 def main():
-    lexer = gcodeLexer (FileStream("gcode_test"))
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="Parse a G-code file using ANTLR.")
+    parser.add_argument('file', type=str, help="Path to the G-code file")
+    args = parser.parse_args()
+
+    # Load the file specified by the user
+    lexer = gcodeLexer(FileStream(args.file))
     token_stream = CommonTokenStream(lexer)
     parser = gcodeParser(token_stream)
     visitor = gcodeVisitor()
 
-   
+    # Parse and visit the file
     while True: 
         tree = parser.start()
         if tree.expr() == None:
@@ -22,3 +29,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
